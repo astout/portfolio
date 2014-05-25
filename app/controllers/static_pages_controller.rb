ebd330c7c135a2ab97334e854a75f85f5bbc458d
Alex Stout <astoutj@gmail.com>
2014-05-25 13:10:52 -0600

    added pages

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,16 @@
+class StaticPagesController < ApplicationController
+  def home
+  end
+
+  def games
+  end
+
+  def software
+  end
+
+  def web
+  end
+
+  def contact
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Portfolio::Application.routes.draw do
+  get "static_pages/home"
+  get "static_pages/games"
+  get "static_pages/software"
+  get "static_pages/web"
+  get "static_pages/contact"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
